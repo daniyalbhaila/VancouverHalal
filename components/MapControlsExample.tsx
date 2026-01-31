32b469baa680c581292911594601a3fd3a817c08
Daniyal Bhaila <43946521+daniyalbhaila@users.noreply.github.com>
2026-01-31 10:33:24 -0800

    Add map controls example component

---
+++ components/MapControlsExample.tsx
@@ -0,0 +1,18 @@
+'use client';
+
+import { Map, MapControls } from '@/components/ui/map';
+
+export function MapControlsExample() {
+    return (
+        <div className="h-[400px] w-full">
+            <Map center={[2.3522, 48.8566]} zoom={11}>
+                <MapControls
+                    position="bottom-right"
+                    showZoom
+                    showCompass
+                    showLocate
+                />
+            </Map>
+        </div>
+    );
+}
